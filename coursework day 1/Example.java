156a544398e61062d1a26dce765e1f552394d5ac
yetimeh <senukabandara2006@gmail.com>
2024-10-08 21:27:27 +0530

    Add coursework day 1

---
+++ coursework day 1/Example.java
@@ -0,0 +1,45 @@
+import java.util.*;
+class Example {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("\t---------------------------------------------------------------------------------");
+        System.out.println("\t||\t\t                                                                ");
+        System.out.println("\t||\t\t	                                                                ");       
+        System.out.println("\t||\t\t                           ~'%@@%'~                             ");       
+        System.out.println("\t||\t\t                         ~@@@@@@@@@@~                           ");       
+        System.out.println("\t||\t\t    '~#@@#'             ~@@@@@@@@@@@@~                          ");       
+        System.out.println("\t||\t\t   '@@@@@@@@            @@@@@@@@@@@@@@%                         ");       
+        System.out.println("\t||\t\t   @@@@@@@@@@           ~@@@@@@@@@@@@@~                         ");       
+        System.out.println("\t||\t\t   '@@@@@@@@'            '@@@@@@@@@@@~                          ");       
+        System.out.println("\t||\t\t    .;#@@#,.               -'%@@%'-                             ");       
+        System.out.println("\t||\t\t  ~#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                                ");       
+        System.out.println("\t||\t\t *@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@        '#@@@                        ");       
+        System.out.println("\t||\t\t @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@   :'@@@@@@@                             ");       
+        System.out.println("\t||\t\t @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                                ");       
+        System.out.println("\t||\t\t @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                                ");       
+        System.out.println("\t||\t\t @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                                ");       
+        System.out.println("\t||\t\t @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                                ");       
+        System.out.println("\t||\t\t  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@    :'@@@@@@@                          ");       
+        System.out.println("\t||\t\t   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@          '#@@@                     ");       
+        System.out.println("\t||\t\t     *@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                            ");       
+        System.out.println("\t||\t\t               ,#@@@@++@@@@%                  ");       
+        System.out.println("\t||\t\t              -@@@@%     %@@@~           ");       
+        System.out.println("\t||\t\t              ~%%~         ~%%~       ");       
+        System.out.println("\t||\t\t                                 ");       
+        System.out.println("\t||\t\t                                 ");       
+        System.out.println("\t||\t\t                                 ");       
+        System.out.println("\t||\t\t                                 ");   
+        System.out.println("\t||\t\t                                 ");       
+        System.out.println("\t||\t\t                                 ");       
+        System.out.println("\t||\t\t                                 ");       
+        System.out.println("\t||\t\t                                 ");       
+        System.out.println("\t||\t\t                                 ");       
+        System.out.println("\t||\t\t                                 ");       
+        System.out.println("\t||\t\t                                 ");       
+
+        
+        System.out.println("\t---------------------------------------------------------------------------------");
+    
+    }
+}
