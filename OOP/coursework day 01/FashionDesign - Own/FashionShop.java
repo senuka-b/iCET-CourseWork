6d2707ac5297b0ce808333e1f2c7b7534ea64c84
yetimeh <senukabandara2006@gmail.com>
2024-10-24 10:20:01 +0530

    Create ConsoleWriter Add Home Page Add integer input

--- OOP/coursework day 01/FashionDesign - Own/FashionShop.java
+++ OOP/coursework day 01/FashionDesign - Own/FashionShop.java
@@ -1,7 +1,70 @@
-
+import java.util.*;
 public class FashionShop {
-    public static void main(String args[]) {
+
+    private static Scanner input = new Scanner(System.in);
+
+    public static void homePage() {
+        clearConsole();
+   
         
+        int choice = input.nextInt();
+        
+        switch (choice) { 
+            case 1:
+                //laceOrder();
+                
+                break;
+            case 2:
+                //searchCustomer();    
+
+                break;
+            case 3:
+                //searchOrder();
+
+                break;
+            case 4:
+                //viewReports();
+                
+                break;
+            case 5:
+                //setOrderStatus();
+
+                break;
+            case 6:
+                //deleteOrder();
+                
+                break;
+            
+            default:
+                return;
+   
+        }
+
+        homePage();
+    
+    }
+
+    public static void main(String args[]) {
+        homePage();
+
+        clearConsole();
+
+        System.out.println("Goodbye!");
+    }
+
+    public final static void clearConsole() {
+        try {
+            final String os = System.getProperty("os.name");
+                if (os.contains("Windows")) {
+                    new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+                } else {
+                    System.out.print("\033[H\033[2J");
+                    System.out.flush();
+                }
+        } catch (final Exception e) {
+                e.printStackTrace();
+                // Handle any exceptions.
+        }
     }
     
 }
